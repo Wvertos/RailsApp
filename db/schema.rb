dfee790462e9535dc497a9047b8716b5a1347185
Wvertos <wvertos@gmail.com>
2022-09-08 09:22:56 +0200

    second migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_083213) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_08_072110) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.date "date"
