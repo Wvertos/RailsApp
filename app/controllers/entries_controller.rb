57e436d48d6d2ee77e6d2791cd6955b794f68e30
Wvertos <wvertos@gmail.com>
2022-09-14 11:00:27 +0200

    Entries_new_create

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,22 @@
 class EntriesController < ApplicationController
     def index
-        @entries = Entry.all.sort_by{|entry| entry.date}
+        @entries = Entry.all.sort_by{|entry|}
     end
 
     def show
         puts params
         @entry = Entry.find(params["id"])
       end
+
+      def new
+      end
+
+      def create
+        puts "......, #{params}"
+        entry_params = params["entry"].permit("title", "contents")
+        puts "......, #{params["entry"]}"
+        entry = Entry.create(entry_params)
+        redirect_to(entry_path(entry))
+      end
+      
 end
